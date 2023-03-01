fbdaadbd9ac41f4849e807f0ef0b089fccfb2e7f
zigal0 <zigal0.bc@gmail.com>
2023-03-01 22:19:09 +0300

    [feature_1] makefile & migrations added

---
+++ Makefile
@@ -0,0 +1,26 @@
+# WORK_DIR=$(shell pwd)
+# export PYTHONPATH="$(WORK_DIR)" &&
+
+# MIGRATIONS
+.PHONY: local-migration-up
+local-migration-up:
+	echo "local migration up is processing..."
+	cd bookkeeper/database && python3 migration.py up
+
+.PHONY: local-migration-down
+local-migration-down:
+	echo "local migration down is processing..."
+	cd bookkeeper/database && python3 migration.py down
+
+# SETUP
+.PHONY: setup
+setup:
+	poetry install
+
+# TEST
+.PHONY: lint
+lint:
+	poetry run pytest --cov
+	poetry run mypy --strict bookkeeper
+	poetry run pylint bookkeeper
+	poetry run flake8 bookkeeper
---
+++ bookkeeper/database/migration.py
@@ -0,0 +1,138 @@
+import sys
+import os
+import sqlite3
+
+# TODO: Требуется мета таблица, для слежки за миграциями.
+# from dataclasses import dataclass, field
+# from datetime import datetime
+#
+#
+# CREATE_META_MIGRATION_TABLE = """CREATE TABLE IF NOT EXISTS meta_migration (
+#     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+#     "version_id" INTEGER UNIQUE NOT NULL,
+#     "applied_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+# );"""
+#
+# GET = """SELECT * FROM meta_migration WHERE version_id = 1;"""
+#
+#
+# @dataclass(slots=True)
+# class MetaMigration:
+#     """
+#     Мета информация о миграциях.
+#     id - счетчик миграций
+#     version_id - id маграции
+#     applied_at - время применения миграции
+#     """
+#     id: int
+#     version_id: int
+#     applied_at: datetime = field(default_factory=datetime.now)
+
+
+DB_FILE = 'bookkeeper.db'
+AVAILABLE_MODES = ['up', 'down']
+MIGRATION_DIR = "migration"
+
+
+class Colors:
+    INFO = '\033[94m'
+    SUCCESS = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    DEFAULT = '\033[0m'
+
+
+def print_fail_msg(msg: str):
+    print(f"{Colors.FAIL}FAIL: " + msg + f"{Colors.DEFAULT}")
+
+
+def print_warning_msg(msg: str):
+    print(f"{Colors.WARNING}WARNING: " + msg + f"{Colors.DEFAULT}")
+
+
+def print_info_msg(msg: str):
+    print(f"{Colors.INFO}INFO: " + msg + f"{Colors.DEFAULT}")
+
+
+def print_success_msg(msg: str):
+    print(f"{Colors.SUCCESS}SUCCESS: " + msg + f"{Colors.DEFAULT}")
+
+
+def execute_query(query: str):
+    with sqlite3.connect(DB_FILE) as con:
+        cur = con.cursor()
+        cur.execute(query)
+
+    con.close()
+
+
+def split_sql_queries(sql_queries: str):
+    return [query.strip() for query in sql_queries.split(';')]
+
+
+# run_migration runs 1 file of migrations
+def run_migration(file_name):
+    file_name_parts = file_name.split('_')
+
+    try:
+        version_id = int(file_name_parts[0])
+    except ValueError:
+        print_fail_msg('Incorrect migration name')
+
+        sys.exit()
+
+    path = MIGRATION_DIR + "/" + file_name
+    with open(path, mode='r') as file:
+        sql_script = file.read()
+
+    up_down_scripts = sql_script.split('-- down')
+
+    if mode == 'up':
+        queries = split_sql_queries(up_down_scripts[0])
+    else:
+        queries = split_sql_queries(up_down_scripts[1])
+
+    for query in queries:
+        execute_query(query)
+
+    print_info_msg(f"Migration with id = {version_id} was applied with mode {mode}")
+
+
+# main script
+if len(sys.argv) != 2:
+    print_fail_msg("You need to provide one of next options: " + str(AVAILABLE_MODES))
+    sys.exit()
+
+mode = sys.argv[1]
+
+if mode not in AVAILABLE_MODES:
+    print_fail_msg("Mode " + mode + " is incorrect")
+    sys.exit()
+
+all_file_names = os.listdir(MIGRATION_DIR)
+migrations = sorted([file_name for file_name in all_file_names
+                     if file_name.endswith(".sql")])
+
+for migration_name in migrations:
+    run_migration(migration_name)
+
+print_success_msg("All migration were applied")
+
+# Try to create fast schemas
+# from bookkeeper.models.expense import Expense
+# from bookkeeper.models.budget import Budget
+# from bookkeeper.models.category import Category
+#
+#
+# def create_db_schemas():
+#     for cls in [Expense, Budget, Category]:
+#         table_name = cls.__name__.lower()
+#         fields = get_annotations(cls, eval_str=True)
+#         fields.pop('pk')
+#         db_fields = ', '.join(fields.keys())
+#
+#         with sqlite3.connect(DB_FILE) as con:
+#             cur = con.cursor()
+#             cur.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({db_fields})")
+#
+#         con.close()
